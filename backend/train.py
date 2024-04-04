0134453dbd06768955e42d4df8b15072f7f872d6
Julianna Harwood <julianna.harwood@latimes.com>
2024-04-04 16:00:29 -0700

    integrate with website

--- backend/train.py
+++ backend/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def load_data():
     # Load data from CSV file into a DataFrame
@@ -21,7 +22,14 @@ def feature_engineer(pickle_clean):
     pickle_clean['Game Length'] = pickle_clean.groupby(['Date'])['Game End Times'].diff().fillna(pickle_clean['Game End Times'] - pickle_clean['Court Time'])
     return(pickle_clean)
 
-def get_data(pickle_clean, ind):
-    return(pickle_clean.iloc[ind])
+def get_data(pickle_clean, game_length):
+    sorted = pickle_clean.sort_values(by='Game Length')
+    game_lengths = sorted['Game Length']
+    if game_length == "short":
+        return("Shortest game was", str(game_lengths.iloc[0]))
+    elif game_length == "medium":
+        return("Average game length is", str(np.mean(game_lengths)))
+    elif game_length == "long":
+        return("Longest game was", str(game_lengths.iloc[-1]))
 
 
