4185cd42077e5c83bf433f59acb800525d22ed0c
Julianna Harwood <julianna.harwood@latimes.com>
2024-04-05 11:25:35 -0700

    Revert "integrate with website"
    This reverts commit 0134453dbd06768955e42d4df8b15072f7f872d6.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, json, redirect, url_for
+from flask import Flask, render_template, request, jsonify, json
 from backend import train
 import pandas as pd
 
@@ -8,27 +8,7 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    # Get the password entered by the user
-    password = request.form['password']
-    
-    # Hardcoded password (replace this with your actual password)
-    correct_password = 'jojosiwa'
-    
-    # Validate the password
-    if password == correct_password:
-        # Redirect to the protected page if the password is correct
-        return redirect(url_for('pickleball_predictor'))
-    else:
-        # Redirect back to the login page with a message if the password is incorrect
-        return redirect(url_for('index'))
-
-@app.route('/pickleball-predictor')
-def pickleball_predictor():
-    return render_template('flask_index.html')
-
-@app.route('/pickleball-predictor/show_data', methods=['POST'])
+@app.route('/show_data', methods=['POST'])
 def show_data():
     pickle_raw = train.load_data()
     pickle_clean = train.clean_data(pickle_raw)
@@ -36,9 +16,9 @@ def show_data():
     data = request.get_json()
     selected_value = data['value']
     # Process the selected value as needed
-    final_data = train.get_data(pickle_clean, selected_value)
-    print(final_data)
-    return jsonify(str(final_data))
+    final_data = train.get_data(pickle_clean, int(selected_value))
+    print(final_data['Game Length'])
+    return jsonify(str(final_data['Game Length']))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/train.py
+++ backend/train.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 def load_data():
     # Load data from CSV file into a DataFrame
@@ -22,14 +21,7 @@ def feature_engineer(pickle_clean):
     pickle_clean['Game Length'] = pickle_clean.groupby(['Date'])['Game End Times'].diff().fillna(pickle_clean['Game End Times'] - pickle_clean['Court Time'])
     return(pickle_clean)
 
-def get_data(pickle_clean, game_length):
-    sorted = pickle_clean.sort_values(by='Game Length')
-    game_lengths = sorted['Game Length']
-    if game_length == "short":
-        return("Shortest game was", str(game_lengths.iloc[0]))
-    elif game_length == "medium":
-        return("Average game length is", str(np.mean(game_lengths)))
-    elif game_length == "long":
-        return("Longest game was", str(game_lengths.iloc[-1]))
+def get_data(pickle_clean, ind):
+    return(pickle_clean.iloc[ind])
 
 
